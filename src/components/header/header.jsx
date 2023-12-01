ea570a9939fb907905141f1500777af8083597a5
Noktima <aleksej.konovalov.2000@gmail.com>
2023-12-01 21:14:01 +0700

    Сделал переключатель тёмной/светлой темы

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from 'react';
 import styles from './style.header.css'
 import { Link } from "react-router-dom"
+import Toggle from "../react-toggle-master/component"
 
 
 const Header = () => {
+    
+    const [isDark, setIsDark] = useState(true);
 
+    const DarkModeToggle = () => {
+        const [isDark, setIsDark] = useState(true);
+    }
 
+    useEffect(() => {
+        if (isDark) {
+          document.body.classList.add('dark');
+        } else {
+          document.body.classList.remove('dark');
+        }
+      }, [isDark]); 
 
-
-
+      
 
     return (
         <div>
@@ -17,6 +30,14 @@ const Header = () => {
                     <Link to='/Help' className='link'>HELP</Link>
                    <Link to='/Негр' className='link'>НЕГР</Link>
                 </nav>
+                <div id='themeDiv'>
+                    <Toggle
+                        checked={isDark}
+                        onChange={({ target }) => setIsDark(target.checked)}
+                        icons={{ checked: "🌙" }}
+                        aria-label="Dark mode toggle"
+                    />
+                </div>
             </header>
         </div>
     )
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,19 +1,21 @@
+import { useEffect, useState } from "react";
+import Toggle from "../react-toggle-master/component"
 
 
 const Home = () => {
 
 
 
-
     return (
         <div>
             <div id='Darkdurka'>
-                <h1>Добро пожаловать на сервер Ебланов</h1>
+                <h1>Добро пожаловать</h1>
             </div>
             <div id='about'>
                 <h2>О нас</h2>
-                <h3>Мы - главные ебланы такой ебанутой игры, как Mindustry, просим относиться к нам с пониманием.</h3>
+                <h3></h3>
             </div>
+
         </div>
     )
 }
