8998ef17720ead94db3fddb1100df137acb5d409
Noktima <aleksej.konovalov.2000@gmail.com>
2023-12-03 21:55:30 +0700

    Made comments

--- src/components/error-page/error-page.jsx
+++ src/components/error-page/error-page.jsx
@@ -3,11 +3,11 @@ import styles from "./style.error-page.css"
 
 const Error = () => {
 
-
-
+        
     return (
         <div id='errorPage'>
             <h1>ERROR 404</h1>
+            <img src={require('./images/404.png')} />
             <h3>Not found</h3>
         </div>
     )
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,22 +4,10 @@ import { Link } from "react-router-dom"
 
 
 const Header = () => {
-    
+
+    // Смена темы
+
     const [isDark, setIsDark] = useState();
-
-
-
-
-    let changeImg = () => {
-      if (isDark) {
-        document.all.themeButton.src = require("./themes/lightTheme.png")
-      } else {
-        document.all.themeButton.src = require("./themes/darkTheme.png")
-      }
-      console.log(isDark)
-    }
-
-  
 
     useEffect(() => {
         if (isDark) {
@@ -29,17 +17,29 @@ const Header = () => {
         }
       }, [isDark]); 
 
-    let darkTheme
+
+
+    // Смена картинки
+    let changeImg = () => {
+      if (isDark) {
+        document.all.themeButton.src = require("./themes/lightTheme.png")
+      } else {
+        document.all.themeButton.src = require("./themes/darkTheme.png")
+      }
+      console.log(isDark)
+    }
+
 
     return (
             <header id='header'>
                 <nav id="navBar">
-                   <Link to='/' className='link'>Home</Link>
+                    <Link to='/' className='link'>Home</Link>
                     <Link to='/Help' className='link'>HELP</Link>
-                   <Link to='/Негр' className='link'>НЕГР</Link>
+                    <Link to='/Негр' className='link'>НЕГР</Link>
                 </nav>
                   <div id='themeDivDiv'>
                       <div id='themeDiv'>
+                        {/* Кнопка смены темы */}
                       <img id='themeButton' src={require('./themes/lightTheme.png')} onClick={() => {
                       setIsDark(!isDark);
                       changeImg()
